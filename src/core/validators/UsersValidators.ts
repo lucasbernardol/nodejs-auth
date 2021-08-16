0972b5ae5622de8d166c4d2fa924f347a79fde02
José Lucas <jose.lukass03@gmail.com>
2021-08-16 19:24:23 -0300

    feat: create Joi schemas for users

---
+++ src/core/validators/UsersValidators.ts
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+
+/**
+ * @constant
+ */
+const account = {
+  body: {
+    signUpSchema: Joi.object({
+      name: Joi.string().min(5).max(120).trim().required(),
+      username: Joi.string().min(5).max(120).trim().lowercase().required(),
+      email: Joi.string().email().trim().lowercase().min(8).required(),
+      password: Joi.string().min(6).max(32).trim().lowercase().required(),
+      repeat: Joi.ref('password'),
+    }),
+  },
+};
+
+export { account };
