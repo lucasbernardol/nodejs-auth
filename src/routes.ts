9584b75576e894c7cf6c74ab0132bfc0581fce3c
José Lucas <jose.lukass03@gmail.com>
2021-08-16 19:25:31 -0300

    feat: using Celebrate on routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 
 import { MainController } from './core/controllers/MainController';
 import { UsersControllers } from './core/controllers/UsersControllers';
+
+import { account } from './core/validators/UsersValidators';
+
+const { signUpSchema } = account.body;
 
 const routes = Router();
 
@@ -20,6 +25,10 @@ const usersControllers = new UsersControllers();
 routes.get('/users', usersControllers.all);
 routes.get('/users/:id', usersControllers.findId);
 
-routes.post('/users', usersControllers.create);
+routes.post(
+  '/users',
+  celebrate({ body: signUpSchema }),
+  usersControllers.create
+);
 
 export { routes };
