4de7ca02454138fd33fe0ee83c0230c8cb53ece5
José Lucas <jose.lukass03@gmail.com>
2021-08-16 17:52:03 -0300

    refactor: add pagination in the user list

--- src/core/controllers/UsersControllers.ts
+++ src/core/controllers/UsersControllers.ts
@@ -6,13 +6,16 @@ import { UsersServices } from '../services/UsersServices';
  * @class UsersControllers
  */
 class UsersControllers {
-  constructor(private services = new UsersServices()) {}
-
   async all(request: Request, response: Response, next: NextFunction) {
     try {
+      const { page, limit } = request.query;
+
       const services = new UsersServices();
 
-      const { accounts, meta } = await services.list();
+      const { accounts, meta } = await services.list({
+        page: Number(page) || 1,
+        limit: Number(limit) || 10,
+      });
 
       return response.json({ meta, accounts });
     } catch (error) {
--- src/core/services/UsersServices.ts
+++ src/core/services/UsersServices.ts
@@ -2,19 +2,34 @@ import { classToPlain } from 'class-transformer';
 import { getCustomRepository } from 'typeorm';
 
 import { UsersRepositories } from '../repositories/UsersRepositories';
+import { paginate } from '../utilities/paginate';
+
+export interface Pagination {
+  limit: number;
+  page: number;
+}
 
 /**
  * @class UsersServices
  */
 class UsersServices {
-  async list() {
+  async list(options: Pagination) {
+    const { limit, page } = options;
+
     const usersRepositories = getCustomRepository(UsersRepositories);
 
-    const accountResults = await usersRepositories.find();
+    const total = await usersRepositories.count();
+
+    const { offSet, range, pagination } = paginate({ total, page, limit });
+
+    const accountResults = await usersRepositories.find({
+      take: pagination.limit,
+      skip: offSet,
+    });
 
     const accounts = accountResults.map((account) => classToPlain(account));
 
-    return { accounts, meta: {} };
+    return { accounts, meta: { pagination, range } };
   }
 }
 
