11d64c3d8ef2f675c18402c9d86f7bdd5911e61a
José Lucas <jose.lukass03@gmail.com>
2021-08-24 17:18:46 -0300

    refactor: update Joi validation schemas

--- src/core/validators/UsersValidators.ts
+++ src/core/validators/UsersValidators.ts
@@ -10,13 +10,18 @@ const account = {
       username: Joi.string().min(5).max(120).trim().lowercase().required(),
       email: Joi.string().email().trim().lowercase().min(8).required(),
       password: Joi.string().min(6).max(32).trim().lowercase().required(),
-      repeat: Joi.ref('password'),
+      repeat: Joi.string().valid(Joi.ref('password')),
     }),
 
     signInSchema: Joi.object({
       email: Joi.string().email().trim().lowercase().min(8).required(),
       password: Joi.string().min(6).max(32).trim().lowercase().required(),
-      repeat: Joi.ref('password'),
+      repeat: Joi.string().valid(Joi.ref('password')),
+    }),
+
+    deleteSchema: Joi.object({
+      password: Joi.string().min(6).max(32).trim().lowercase().required(),
+      repeat: Joi.string().valid(Joi.ref('password')),
     }),
   },
 };
