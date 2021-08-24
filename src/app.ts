9a1496b1ab8f39ab3ea5a57176f4dc5386efe8a5
José Lucas <jose.lukass03@gmail.com>
2021-08-24 18:48:29 -0300

    feat: using hpp middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import hpp from 'hpp';
 
 import { routes } from './routes';
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(hpp({ checkBody: false }));
 
 app.use(cors());
 app.use(morgan('dev'));
