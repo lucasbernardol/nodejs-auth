3004ab9e6648b566aaca04085a637f1fe1728205
José Lucas <jose.lukass03@gmail.com>
2021-08-25 15:20:32 -0300

    feat: create a method to forget passwords

--- src/core/controllers/AlterControllers.ts
+++ src/core/controllers/AlterControllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
+import { MailService } from '../services/SendMailService';
 import { AlterServices } from '../services/AlterServices';
 
 /**
@@ -14,13 +15,37 @@ class AlterControllers {
 
       const services = new AlterServices();
 
-      const { updated } = await services.change({
-        id,
-        oldPassword,
-        password,
-      });
+      const { updated } = await services.change({ id, oldPassword, password });
 
       return response.json({ id, updated });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async forgot(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { email } = request.body;
+
+      const services = new AlterServices();
+
+      const { updated, token } = await services.forgot(email);
+
+      const mail = new MailService();
+
+      /**
+       * - nodemailer options
+       */
+      const options = {
+        to: email,
+        subject: 'Esqueceu sua senha?',
+        variables: { token },
+        template: 'forgot',
+      };
+
+      await mail.sendMessage(options);
+
+      return response.json({ updated });
     } catch (error) {
       return next(error);
     }
--- src/core/services/AlterServices.ts
+++ src/core/services/AlterServices.ts
@@ -1,6 +1,11 @@
+import dayjs from 'dayjs';
+import { classToPlain } from 'class-transformer';
+
 import { getCustomRepository } from 'typeorm';
 import { BadRequest } from 'http-errors';
 import { compare, hash } from 'bcryptjs';
+
+import { randomBytesAsync } from '../utilities/randomBytesAsync';
 
 import { UsersRepositories } from '../repositories/UsersRepositories';
 
@@ -14,10 +19,44 @@ export interface UpdateContext {
  * @class
  */
 class AlterServices {
+  /**
+   * - bcrypt, salt length
+   */
   private salt: number = 8;
 
+  /**
+   * - token size
+   */
+  private size: number = 22;
+
+  async forgot(email: string) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const account = await usersRepositories.findOne({ email });
+
+    if (!account) throw new BadRequest();
+
+    const { id } = account;
+
+    /**
+     * - ramdomBytes async
+     */
+    const token = await randomBytesAsync(this.size);
+
+    const expires = dayjs().add(30, 'minutes').toDate();
+
+    const { affected } = await usersRepositories.update(id, { token, expires });
+
+    return {
+      user: classToPlain(account),
+      token,
+      expires,
+      updated: affected,
+    };
+  }
+
   async change(context: UpdateContext) {
-    const { id, oldPassword, password } = context;
+    const { id, oldPassword, password: plain } = context;
 
     const usersRepositories = getCustomRepository(UsersRepositories);
 
@@ -30,15 +69,15 @@ class AlterServices {
     if (!isMatch) throw new BadRequest();
 
     /**
-     * - hash, update password
+     * - hash, update passwords
      */
-    const passwordHash = await hash(password, this.salt);
+    const password = await hash(plain, this.salt);
 
-    const { affected: updated } = await usersRepositories.update(id, {
-      password: passwordHash,
-    });
+    const { affected } = await usersRepositories.update(id, { password });
 
-    return { id, updated };
+    return {
+      updated: affected,
+    };
   }
 }
 
