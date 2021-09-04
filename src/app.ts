5a2302951208f907535486e0c1b8b237a9f935ec
José Lucas <jose.lukass03@gmail.com>
2021-09-04 18:25:59 -0300

    refactor: remove morgan config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
 import hpp from 'hpp';
 
 import { routes } from './routes';
@@ -12,20 +11,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(hpp({ checkBody: false }));
-
 app.use(cors());
-app.use(morgan('dev'));
+app.use(hpp({ checkBody: false }));
 
 app.use(routes);
 
-/**
- * Validations
- * - Not found
- * - Validation errors (celebrate, Joi)
- * - Http (errors)
- */
 app.use(celebrateValidation({ setDetails: false }));
 
 app.use(NotFound(), HttpHandler());
